da744c643883a5160ab41158b59a256fb1fa9e8f
mac <innakar@gmail.com>
2022-12-03 19:51:35 -0500

    ok

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println("bean == bean2: " + (bean == bean2));
+
+
+        Cat cat = applicationContext.getBean("Cat!", Cat.class);
+        Cat cat2 = applicationContext.getBean("Cat!", Cat.class);
+        System.out.println("cat == cat2: " + (cat == cat2));
     }
 }
